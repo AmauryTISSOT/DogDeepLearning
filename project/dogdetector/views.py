93806fa820be75627167d835821cac028aceef8c
Amaury TISSOT <tissot.amaury@gmail.com>
2025-08-06 11:42:22 +0200

    Initialize Django project structure with essential files and configurations

---
+++ project/dogdetector/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def accueil(request):
+    return HttpResponse("Bienvenue sur mon site Django")
+
+# Create your views here.
