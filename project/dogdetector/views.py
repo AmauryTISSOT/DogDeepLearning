d4d1f87e0ad91b89e81acf069cce15214f73fa92
Amaury TISSOT <tissot.amaury@gmail.com>
2025-08-06 12:12:23 +0200

    Implement image upload functionality with preview and breed detection placeholder

--- project/dogdetector/views.py
+++ project/dogdetector/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.core.files.storage import default_storage
+import os
 
 def accueil(request):
     return HttpResponse("Bienvenue sur mon site Django")
 
-# Create your views here.
+def upload_image(request):
+    if request.method == 'POST' and request.FILES.get('image'):
+        image = request.FILES['image']
+        path = default_storage.save('uploads/' + image.name, image)
+
+        # Ici pour appeler l'ia
+        predicted_breed = "Race inconnue (à implémenter)"
+
+        return render(request, 'upload.html', {
+            'uploaded': True,
+            'breed': predicted_breed,
+            'image_url': '/media/' + path
+        })
+    return render(request, 'upload.html')
